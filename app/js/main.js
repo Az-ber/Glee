4f12c2e9e4fea69a5e934e8c75efc5bfc339a8bf
Az-ber <azber7049@gmail.com>
2021-05-21 17:33:26 +0500

    added products, slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function () {
-  
+  $('.slider__items').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  var mixer = mixitup('.products-content');
 });
